220162256c4e207ccd119f4feb16931251a8b6df
unknown <abylai1393@gmail.com>
2023-02-05 16:02:23 +0600

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,16 @@
-import Categories from './components/Categories'
 import Header from './components/Header'
-import PizzaBlock from './components/PizzaBlock'
-import Sort from './components/Sort'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 import './scss/app.scss'
-import { useEffect, useState } from 'react'
-import Skeleton from './components/PizzaBlock/Skeleton'
 
 function App() {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('https://63db9da5a3ac95cec5a618b3.mockapi.io/items')
-      .then(res => res.json())
-      .then(arr => {
-        setItems(arr)
-        setIsLoading(false)
-      })
-  }, [])
-
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
         <div className='container'>
-          <div className='content__top'>
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className='content__title'>Все пиццы</h2>
-          <div className='content__items'>
-            {isLoading
-              ? [...Array(6)].map((_, i) => <Skeleton key={i} />)
-              : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <NotFound />
+          <Home />
         </div>
       </div>
     </div>
